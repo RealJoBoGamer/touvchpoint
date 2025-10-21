aa3e87cf4c5f9c50e4af2a55cc5f50f2ee28836e
JoBo Gamer <78912075+RealJoBoGamer@users.noreply.github.com>
2025-10-21 11:47:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: 'build', // your deploy folder
+    outDir: 'build',
   },
-  base: '/touvchpoint/', // 👈 IMPORTANT: replace with your repo name
+  base: '/touvchpoint/', // 👈 EXACT repo name (case-sensitive)
 })
